8d071a3d34297e86ae3b28a1cc062acb41b5eb02
susu-noob <154180374@qq.com>
2023-05-17 00:49:08 -0600

    fix default optimizer and add group optimizer

--- experimental/plugin/optimizer/__init__.py
+++ experimental/plugin/optimizer/__init__.py
@@ -1,6 +1,6 @@
-
 import os
 from .optimizer import Optimizer
 from . import default_optimizer
+from . import group_optimizer
 
 __all__ =  ["Optimizer"]
--- experimental/plugin/optimizer/default_optimizer.py
+++ experimental/plugin/optimizer/default_optimizer.py
@@ -1,22 +1,14 @@
-from .optimizer import Optimizer
-import transformers
 import torch
+
+from .optimizer import Optimizer
 
 class DefaultOptimizer(Optimizer):
     def __call__(self, model, config):
-        no_decay = ["bias", "layer_norm.weight"]
-        optimizer_grouped_parameters = [
-            {
-                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
-                "weight_decay": 0.1,
-            },
-            {
-                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
-                "weight_decay": 0.0,
-            },
-        ]
-        lr = config.get("lr", 1e-5)
-        #optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=lr)
-        optimizer = torch.optim.SGD(optimizer_grouped_parameters, lr=lr)
+
+        optimizer_name = config.get("name", "SGD")
+        optimizer_config = config.get("config", {})
+        optimizer_type = eval("torch.optim.%s"%(optimizer_name))
+
+        optimizer = optimizer_type(**optimizer_config)
 
         return optimizer
