309553dc1b5182aa3df0d82b9a8b7d459bb8459b
susu-noob <154180374@qq.com>
2023-05-18 05:50:40 -0600

    polish dataprocesser

--- experimental/plugin/dataprocesser/__init__.py
+++ experimental/plugin/dataprocesser/__init__.py
@@ -1,6 +1,5 @@
-
-import os
 from .dataprocesser import DataProcesser
-from . import default_dataprocesser
+from . import wikitext_processer
+from . import dolly_2_processer
 
 __all__ =  ["DataProcesser"]
