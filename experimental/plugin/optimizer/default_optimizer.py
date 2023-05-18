c201cb94c2536878837c7bf70baea81e8d1ec394
susu-noob <154180374@qq.com>
2023-05-17 19:35:29 -0600

    fix default optimizer

--- experimental/plugin/optimizer/default_optimizer.py
+++ experimental/plugin/optimizer/default_optimizer.py
@@ -9,6 +9,10 @@ class DefaultOptimizer(Optimizer):
         optimizer_config = config.get("config", {})
         optimizer_type = eval("torch.optim.%s"%(optimizer_name))
 
-        optimizer = optimizer_type(**optimizer_config)
+        optimizer_grouped_parameters = self.get_grouped_parameters(model, config)
+        optimizer = optimizer_type(optimizer_grouped_parameters, **optimizer_config)
 
         return optimizer
+
+    def get_grouped_parameters(self, model, config):
+        return model.parameters()
