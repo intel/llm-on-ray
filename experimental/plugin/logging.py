fb2316b549bbf02a36c9c3bd0c69408d41dc3db2
susu-noob <154180374@qq.com>
2023-05-17 19:48:28 -0600

    add fault tolerance to global logger

--- experimental/plugin/logging.py
+++ experimental/plugin/logging.py
@@ -1,5 +1,7 @@
+import sys
 import logging
 import logging.config
+import traceback
 import functools
 
 from .config import Config
@@ -10,11 +12,20 @@ config = Config()
 
 logging_config_file = config.get("logging_config_file", None)
 if logging_config_file is not None:
-    logging.config.fileConfig(logging_config_file)
+    try:
+        logging.config.fileConfig(logging_config_file)
+    except Exception as e:
+        traceback.print_exc()
+        exit(1)
+    
 
 logging_config = config.get("logging_config", None)
 if logging_config is not None:
-    logging.config.dictConfig(logging_config)
+    try:
+        logging.config.dictConfig(logging_config)
+    except Exception as e:
+        traceback.print_exc()
+        exit(1)
 
 logger_name = config.get("logger_name", "root")
 if config.get("use_accelerate_log", False):
