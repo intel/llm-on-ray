c5c12bb66e2961f9348418c959317fcc4103a194
zhangjian94cn <jian2.zhang@intel.com>
2023-06-01 09:56:35 +0000

    [rlhf] add __init__.py for agentenv module

---
+++ experimental/plugin/agentenv/__init__.py
@@ -0,0 +1,9 @@
+import os
+from .agentenv import AgentEnv
+from ..common import import_all_module
+
+realpath = os.path.realpath(__file__)
+basedir = os.path.dirname(realpath)
+import_all_module(basedir, "plugin.agentenv")
+
+__all__ =  ["AgentEnv"]
