da240e6596a97f78f1146e5a982da41e825541a6
zhangjian94cn <jian2.zhang@intel.com>
2023-06-01 09:59:44 +0000

    [dataset] add split arg for load_from_disk

--- experimental/plugin/dataset/huggingface_dataset.py
+++ experimental/plugin/dataset/huggingface_dataset.py
@@ -8,6 +8,10 @@ class HuggingfaceDataset(Dataset):
         load_from_disk = config.get("load_from_disk", False)
         load_config = config.get("load_config", {})
         if load_from_disk:
-            return datasets.load_from_disk(name, **load_config)
+            if 'split' in load_config:
+                split = load_config.pop('split')
+                return datasets.load_from_disk(name, **load_config)[split]
+            else:
+                return datasets.load_from_disk(name, **load_config)
         else:
             return datasets.load_dataset(name, **load_config)
