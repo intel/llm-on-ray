1cf16039396b53ddcd079ebfb3f7a9776da66009
KepingYan <keping.yan@intel.com>
2023-06-05 18:21:01 +0800

    add the process of prompt

--- Inference/chat_process.py
+++ Inference/chat_process.py
@@ -5,13 +5,12 @@ class ChatModel:
     MEANINGLESS_WORDS = ['<pad>', '</s>', '<|endoftext|>', '<br>']
     stop_words = ["<human>"]
 
-    def __init__(self, human_id, bot_id, stop_words, prompt_prefix="", generation_config=None) -> None:
+    def __init__(self, intro, human_id, bot_id, stop_words) -> None:
+        self.intro = intro
         self.human_id = human_id
         self.bot_id = bot_id
         self.stop_words = stop_words
         self.MEANINGLESS_WORDS.extend(self.stop_words)
-        self.prompt_prefix = prompt_prefix
-        self._generation_config = generation_config
 
     def prepare_prompt(self, messages: list):
         """Prepare prompt from history messages."""
@@ -29,11 +28,13 @@ class ChatModel:
 
     def convert_output(self, output: str):
         """Convert the model output to final answer."""
-        bot_turn = output.split(self.human_id)[0]
-        bot_turn = bot_turn.split(self.bot_id)[0]
+        if self.human_id != "":
+            output = output.split(self.human_id)[0]
+        if self.bot_id != "":
+            output = output.split(self.bot_id)[0]
         for word in self.MEANINGLESS_WORDS:
-            bot_turn = bot_turn.replace(word, "")
-        text = bot_turn.strip()
+            output = output.replace(word, "")
+        text = output.strip()
         # remove partial human_id or bot id
         if '\n' in text and (self.human_id.startswith(text[text.rfind('\n')+1:]) or
                              self.bot_id.startswith(text[text.rfind('\n')+1])):
@@ -44,24 +45,30 @@ class ChatModel:
         """Generate response based on messages."""
         prompt = self.prepare_prompt(messages)
         return prompt
-        
-
+    
 class ChatModelGptJ(ChatModel):
-    def __init__(self, human_id, bot_id, stop_words, prompt_prefix="", generation_config=None):
-        super().__init__(human_id, bot_id, stop_words, prompt_prefix, generation_config)
+    def __init__(self, intro, human_id, bot_id, stop_words):
+        super().__init__(intro, human_id, bot_id, stop_words)
 
     def prepare_prompt(self, messages: list):
         """Prepare prompt from history messages."""
-        prompt = self.prompt_prefix
+        prompt = self.intro
         for msg in messages:
             role, content = msg["role"], msg["content"]
             if role == "user":
-                prompt += f"{self.human_id}:\n{content}\n"
+                if self.human_id != "":
+                    prompt += f"{self.human_id}:\n{content}\n"
+                else:
+                    prompt += f"{content}\n"
             elif role == "assistant":
-                prompt += f"{self.bot_id}:\n{content}\n"
+                if self.bot_id != "":
+                    prompt += f"{self.bot_id}:\n{content}\n"
+                else:
+                    prompt += f"{content}\n"
             else:
                 prompt += f"### Unknown:\n{content}\n"
-        prompt += f"{self.bot_id}:\n"
+        if self.bot_id != "":
+            prompt += f"{self.bot_id}:\n"
         return prompt
 
 if __name__ == "__main__":
--- Inference/config.py
+++ Inference/config.py
@@ -3,18 +3,32 @@ model1 = {
     "model_id_or_path": "",
     "port": "",     # port for HTTP server
     "name": "",
-    "route_prefix": ""
+    "route_prefix": "",
+    "chat_model": "ChatModelGptJ", # prompt processing, corresponding to chat_process.py
+    "prompt": {
+        "intro": "",
+        "human_id": "",
+        "bot_id": "",
+        "stop_words": []
+    }
 }
 
 all_models = {}
 base_models = {}
 
-gpt2 = {
-    "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt2",
-    "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt2",
+gpt_j_finetuned_52K = {
+    "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt-j-6B-finetuned-52K",
+    "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt-j-6B-finetuned-52K",
     "port": "8000",
-    "name": "gpt2",
-    "route_prefix": "/gpt2"
+    "name": "gpt_j_finetuned_52K",
+    "route_prefix": "/gpt-j-6B-finetuned-52K",
+    "chat_model": "ChatModelGptJ",
+    "prompt": {
+        "intro": "Below is an instruction that describes a task. Write a response that appropriately completes the request. \n",
+        "human_id": "### Instruction",
+        "bot_id": "### Response",
+        "stop_words": ["### Instruction", "# Instruction", "### Question", "##", " ="]
+    }
 }
 
 gpt_j_6B = {
@@ -22,33 +36,62 @@ gpt_j_6B = {
     "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt-j-6B",
     "port": "8000",
     "name": "gpt-j-6B",
-    "route_prefix": "/gpt-j-6B"
+    "route_prefix": "/gpt-j-6B",
+    "chat_model": "ChatModelGptJ",
+    "prompt": {
+        "intro": "",
+        "human_id": "### Instruction",
+        "bot_id": "### Response",
+        "stop_words": ["### Instruction", "# Instruction", "### Question", "##", " ="]
+    }
 }
 
-gpt_j_finetuned_52K = {
-    "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt-j-6B-finetuned-52K",
-    "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt-j-6B-finetuned-52K",
+gpt2 = {
+    "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt2",
+    "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/gpt2",
     "port": "8000",
-    "name": "gpt_j_finetuned_52K",
-    "route_prefix": "/gpt-j-6B-finetuned-52K"
+    "name": "gpt2",
+    "route_prefix": "/gpt2",
+    "chat_model": "ChatModelGptJ",
+    "prompt": {
+        "intro": "",
+        "human_id": "",
+        "bot_id": "",
+        "stop_words": []
+    }
 }
 
-
-opt = {
-    "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/opt-125m",
-    "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/opt-125m",
-    "port": "8000",
-    "name": "opt",
-    "route_prefix": "/opt"
-}
 
 bloom = {
     "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/bloom-560m",
     "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/bloom-560m",
     "port": "8000",
     "name": "bloom",
-    "route_prefix": "/bloom"
+    "route_prefix": "/bloom",
+    "chat_model": "ChatModelGptJ",
+    "prompt": {
+        "intro": "",
+        "human_id": "",
+        "bot_id": "",
+        "stop_words": []
+    }
 }
+
+opt = {
+    "model_id_or_path": "/mnt/DP_disk3/ykp/huggingface/opt-125m",
+    "tokenizer_name_or_path": "/mnt/DP_disk3/ykp/huggingface/opt-125m",
+    "port": "8000",
+    "name": "opt",
+    "route_prefix": "/opt",
+    "chat_model": "ChatModelGptJ",
+    "prompt": {
+        "intro": "",
+        "human_id": "",
+        "bot_id": "",
+        "stop_words": []
+    }
+}
+
 
 all_models["gpt-j-6B-finetuned-52K"] = gpt_j_finetuned_52K
 all_models["gpt-j-6B"] = gpt_j_6B
