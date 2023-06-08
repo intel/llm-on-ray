38444cd01e4bc4abf3d09e12644a98c9e65dfa56
zhangjian94cn <jian2.zhang@intel.com>
2023-06-08 07:38:47 +0000

    [dataset] remove split args for load_from_disk

--- Finetune/plugin/dataset/huggingface_dataset.py
+++ Finetune/plugin/dataset/huggingface_dataset.py
@@ -8,10 +8,6 @@ class HuggingfaceDataset(Dataset):
         load_from_disk = config.get("load_from_disk", False)
         load_config = config.get("load_config", {})
         if load_from_disk:
-            if 'split' in load_config:
-                split = load_config.pop('split')
-                return datasets.load_from_disk(name, **load_config)[split]
-            else:
-                return datasets.load_from_disk(name, **load_config)
+            return datasets.load_from_disk(name, **load_config)
         else:
             return datasets.load_dataset(name, **load_config)
