8a3a72759c47f6ba03c5c5e7475af92084539f22
Fanli Lin <fanli.lin@intel.com>
2023-08-16 15:53:19 +0800

    refactor code for better user experience (#30)
    * add streaming for local mode
    
    * add validation
    
    * add validation
    
    * Update README.md
    
    * fix typo
    
    * use local instead of stream
    
    * minor change
    
    * refactor
    
    * minor change
    
    * rename tokenizer name
    
    * change title
    
    * use os.path.join instead of hard-coding
    
    * tokenizer position
    
    * fix help text
    
    * use 1 preprocess_megatron

--- tools/redpajama_data_processing/preprocess_full.py
+++ tools/redpajama_data_processing/preprocess_full.py
@@ -1,6 +1,6 @@
 """
-this script is for processing redpajama full data on streaming mode and saving to megatron-format.
-Different to `preprocess_data.py`, this script is mainly for performance. 
+this script is for processing redpajama full data and saving to megatron-format.
+Different to `preprocess_data.py`, this script is mainly for performance.
 """
 
 import os
@@ -76,6 +76,18 @@ def get_args():
             ['arxiv', 'book', 'c4', 'common_crawl', 'github', 'stackexchange', 'wikipedia'] \
             by default the value is set to default"
     )
+    group.add_argument(
+        "--tokenizer",
+        type=str,
+        default='EleutherAI/gpt-neox-20b',
+        help="tokenizer name"
+    )
+    group.add_argument(
+        "--eos-text",
+        type=str,
+        default='<|endoftext|>',
+        help="self-defined eos text",
+    )
     group.add_argument('--split-sentences', action='store_true',
                        help='Split documents into sentences.')
     group.add_argument('--keep-newlines', action='store_true',
@@ -105,6 +117,7 @@ def get_args():
     args.output_path = '/home/user/local'
     return args
 
+
 def save_megatron(out_file, idx_file, docs):
     data_builder = MMapIndexedDatasetBuilder(out_file, dtype=np.uint16)
 
@@ -115,102 +128,117 @@ def save_megatron(out_file, idx_file, docs):
     data_builder.finalize(idx_file)
 
 
+def tokenize_batch_nltk(tokenizer_name, eos_tokens, keep_newlines, batch):
+
+    splitter = nltk.data.load("tokenizers/punkt/english.pickle")
+
+    if keep_newlines:
+        splitter = nltk.tokenize.punkt.PunktSentenceTokenizer(
+                train_text = splitter._params,
+                lang_vars = CustomLanguageVars())
+    else:
+        splitter = splitter 
+    
+    samples = batch['text'].tolist()
+
+    ids = []
+    lens = []
+    
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True)
+    eos_ids = tokenizer(eos_tokens)['input_ids']
+    
+    for sample in samples:
+        sample_ids = []
+        for sentence in splitter.tokenize(sample):
+            encoded = tokenizer(sentence, 
+                                truncation=False,
+                                padding=False)['input_ids']
+            if len(encoded) > 0:
+                sample_ids = sample_ids + encoded
+            
+        sample_ids = sample_ids + eos_ids
+        ids.append(sample_ids)
+        lens.append(len(sample))
+
+    tokenized_batch = pd.DataFrame({"tokens": ids, "length": lens})
+    return tokenized_batch 
+
+
+def tokenize_batch(tokenizer_name, eos_tokens, batch):
+
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True)
+        eos_ids = tokenizer(eos_tokens)['input_ids']
+
+        samples = batch['text'].tolist()
+    
+        ids = []
+        lens = []
+    
+        for sample in samples:
+            encoded = tokenizer(sample, 
+                                truncation=False,
+                                padding=False)  
+            sample_id = encoded['input_ids'] + eos_ids
+            ids.append(sample_id)
+            lens.append(len(sample))
+
+        tokenized_batch = pd.DataFrame({"tokens": ids, "length": lens})
+
+        return tokenized_batch
+
+
+def __best_fitting_dtype(vocab_size=None):
+    if vocab_size is not None and vocab_size < 65500:
+        return np.uint16
+    else:
+        return np.int32
+
+
+def save_megatron(output_dir, data_source, task_id, tokenized_batch, vocab_size):
+    save_dir = os.path.join(output_dir, data_source)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    out_file = os.path.join(save_dir, f"{task_id[:20]}.bin")
+    idx_file = os.path.join(save_dir, f"{task_id[:20]}.idx")
+    
+    data_builder = MMapIndexedDatasetBuilder(out_file, dtype=__best_fitting_dtype(vocab_size))
+
+    for doc in tokenized_batch['tokens']:    
+        data_builder.add_item(np.array(doc, dtype=data_builder.dtype))
+    
+    data_builder.end_document()                        
+    data_builder.finalize(idx_file)
+
+
 def main():
     args = get_args()
 
-    output_dir = f'{args.output_path}/{args.output_prefix}'
+    output_dir = os.path.join(args.output_path, args.output_prefix)
     data_source = args.source 
-    eos_tokens = [0]
     keep_newlines = args.keep_newlines
     split_sentences = args.split_sentences
     cache_dir = args.cache_dir 
+    eos_tokens = args.eos_text 
+    tokenizer_name = args.tokenizer
+    vocab_size = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True).vocab_size 
+    
+    # init ray
     ray.init(address='auto')
     pprint(ray.cluster_resources())
     num_nodes = len(ray.nodes())
     parallelism = num_nodes * args.cpu_per_worker
 
-    def preprocess_megatron_sentence(batch: Dict[str, np.ndarray]) -> pd.DataFrame:
-        
-        task_id = ray.get_runtime_context().get_task_id()
-
-        tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neox-20b', use_fast=True)
-        
-        splitter = nltk.data.load("tokenizers/punkt/english.pickle")
-
-        if keep_newlines:
-            splitter = nltk.tokenize.punkt.PunktSentenceTokenizer(
-                    train_text = splitter._params,
-                    lang_vars = CustomLanguageVars())
-        else:
-            splitter = splitter 
-        
-        samples = batch['text'].tolist()
-        
-        ids = []
-        lens = []
-        
-        for sample in samples:
-            sample_ids = []
-            for sentence in splitter.tokenize(sample):
-
-                encoded = tokenizer(sentence, 
-                                    truncation=False,
-                                    padding=False)['input_ids']
-                if len(encoded) > 0:
-                    sample_ids = sample_ids + encoded
-                
-            sample_ids = sample_ids + eos_tokens
-            ids.append(sample_ids)
-            lens.append(len(sample))
-
-        batch = pd.DataFrame({"tokens": ids, "length": lens})
-
-        if not os.path.exists(f"{output_dir}/{data_source}"):
-            os.makedirs(f"{output_dir}/{data_source}")
-
-        out_file = f'{output_dir}/{data_source}/{task_id[:20]}.bin'
-        idx_file = f'{output_dir}/{data_source}/{task_id[:20]}.idx'
-    
-        save_megatron(out_file, idx_file, batch)
-
-        return pd.DataFrame({'task_id': [task_id]})
-    
-
     def preprocess_megatron(batch: Dict[str, np.ndarray]) -> pd.DataFrame:
         
         task_id = ray.get_runtime_context().get_task_id()
-
-        tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neox-20b', use_fast=True)
-
-        samples = batch['text'].tolist()
-        
-        ids = []
-        lens = []
-        
-        for sample in samples:
-            encoded = tokenizer(sample, 
-                                truncation=False,
-                                padding=False)  
-            sample_id = encoded['input_ids'] + eos_tokens
-            ids.append(sample_id)
-            lens.append(len(sample))
-
-        batch = pd.DataFrame({"tokens": ids, "length": lens})
-
-        if not os.path.exists(f"{output_dir}/{data_source}"):
-            os.makedirs(f"{output_dir}/{data_source}")
-
-        out_file = f'{output_dir}/{data_source}/{task_id[:20]}.bin'
-        idx_file = f'{output_dir}/{data_source}/{task_id[:20]}.idx'
-    
-        save_megatron(out_file, idx_file, batch)
+        if split_sentences:
+            tokenized_batch = tokenize_batch_nltk(tokenizer_name, eos_tokens, keep_newlines, batch)
+        else:
+            tokenized_batch = tokenize_batch(tokenizer_name, eos_tokens, batch)
+        save_megatron(output_dir, data_source, task_id, tokenized_batch, vocab_size)
 
         return pd.DataFrame({'task_id': [task_id]})
-    
-    if split_sentences:
-        fn_name = 'preprocess_megatron_sentence'
-    else:
-        fn_name = 'preprocess_megatron'
 
     if not args.local:
         dataset = load_dataset(args.input, data_source, streaming=True)['train']
@@ -225,7 +253,7 @@ def main():
         ray_dataset = ray.data.from_pandas(df)
         ray_dataset = ray_dataset.repartition(parallelism)
 
-        tokenized_data = ray_dataset.map_batches(eval(fn_name), batch_format="numpy", batch_size=None)
+        tokenized_data = ray_dataset.map_batches(preprocess_megatron, batch_format="numpy", batch_size=None)
         tokenized_data.materialize()
 
         print(f"{idx} * {args.load_batch_size} samples were written to disk.")
