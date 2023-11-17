fb64ee1ff62025992f6ceb6bbcac2c136fe101f8
harborn <gangsheng.wu@intel.com>
2023-11-17 09:41:31 +0800

    delete ACCELERATE_TORCH_DEVICE environment vairable when start training (#123)

--- common/torch_config.py
+++ common/torch_config.py
@@ -2,6 +2,7 @@ from ray.train.torch.config import _TorchBackend
 from ray.train.torch.config import TorchConfig as RayTorchConfig
 from ray.train._internal.worker_group import WorkerGroup
 from dataclasses import dataclass
+import os
 import sys
 # The package importlib_metadata is in a different place, depending on the Python version.
 if sys.version_info < (3, 8):
@@ -9,12 +10,14 @@ if sys.version_info < (3, 8):
 else:
     import importlib.metadata as importlib_metadata
 
+
 @dataclass
 class TorchConfig(RayTorchConfig):
 
     @property
     def backend_cls(self):
         return EnableCCLBackend
+
 
 def libs_import():
     """try to import IPEX and oneCCL.
@@ -37,9 +40,18 @@ def libs_import():
             "Please install torch-ccl"
         ) from ccl_not_exist
 
+
+def _del_torch_distributed_env_vars():
+    del os.environ["ACCELERATE_TORCH_DEVICE"]
+
+
 class EnableCCLBackend(_TorchBackend):
 
     def on_start(self, worker_group: WorkerGroup, backend_config: RayTorchConfig):
         for i in range(len(worker_group)):
             worker_group.execute_single_async(i, libs_import)
         super().on_start(worker_group, backend_config)
+
+    def on_training_start(self, worker_group: WorkerGroup, backend_config: RayTorchConfig):
+        super().on_training_start(worker_group, backend_config)
+        worker_group.execute(_del_torch_distributed_env_vars)
