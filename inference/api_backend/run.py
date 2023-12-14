28cf9bb9b62879fba8b074c5faf7f8e4927c1cda
KepingYan <keping.yan@intel.com>
2023-12-14 15:51:24 +0800

    mv content of app.py to run.py

--- inference/api_backend/run.py
+++ inference/api_backend/run.py
@@ -4,9 +4,9 @@ inference_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
 sys.path.append(inference_path)
 from ray import serve
 from run_model_serve import PredictDeployment, _ray_env_key, _predictor_runtime_env_ipex
-from app import RouterDeployment
 from inference_config import all_models
 from plugin.query_client import RouterQueryClient
+from routers.router_app import Router, router_app
 
 
 def router_application(model_ids, hooks=None):
@@ -40,7 +40,18 @@ def router_application(model_ids, hooks=None):
         deployment_map[model_id] = PredictDeployment.options(ray_actor_options=ray_actor_options).bind(model_config)
     merged_client = RouterQueryClient(deployment_map)
 
-
+    RouterDeployment = serve.deployment(
+        route_prefix="/",
+        autoscaling_config={
+            "min_replicas": int(os.environ.get("ROUTER_MIN_REPLICAS", 2)),
+            "initial_replicas": int(os.environ.get("ROUTER_INITIAL_REPLICAS", 2)),
+            "max_replicas": int(os.environ.get("ROUTER_MAX_REPLICAS", 16)),
+            "target_num_ongoing_requests_per_replica": int(
+                os.environ.get("ROUTER_TARGET_NUM_ONGOING_REQUESTS_PER_REPLICA", 200)
+            ),
+        },
+        max_concurrent_queries=1000,  # Maximum backlog for a single replica
+    )(serve.ingress(router_app)(Router))
 
     return RouterDeployment.options(
         autoscaling_config={
@@ -50,7 +61,6 @@ def router_application(model_ids, hooks=None):
             "target_num_ongoing_requests_per_replica": 1,
         }
     ).bind(merged_client)
-
 
 def run(model_ids, route_prefix="/", hooks=None):
     """Run the LLM Server on the local Ray Cluster
@@ -70,8 +80,6 @@ def run(model_ids, route_prefix="/", hooks=None):
     )
     deployment_address = f"http://{host}:8000"
     return deployment_address
-
-
 
 if __name__ == "__main__":
     run(model_ids=["gpt2"])
