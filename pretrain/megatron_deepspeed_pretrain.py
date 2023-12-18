40d566f53d926cf228d55b83f75db6d0386e8137
yuanwu2017 <yuan.wu@intel.com>
2023-12-18 10:08:43 +0800

    Add the HFTonkenizer patch for Model-References (#169)
    Add the HFTokenizer patch
    Add the pretrain_module to invoke different pretrain module
    
    Signed-off-by: yuanwu <yuan.wu@intel.com>

--- pretrain/megatron_deepspeed_pretrain.py
+++ pretrain/megatron_deepspeed_pretrain.py
@@ -36,38 +36,31 @@ def train_func(config: Dict[str, Any]):
             if pretrain_module_name is not None:
                 pretrain_module = importlib.import_module(pretrain_module_name)
             else:
-                pretrain_module = importlib.import_module("pretrain_gpt")
+                pretrain_module = importlib.import_module('pretrain_gpt')
         else:
             raise ImportError("Please set megatron_deepspeed_path in config")
-
-    common.init(config)  # type: ignore
-    megatron_config = config.get("megatron_config", {})
-
-    if hasattr(pretrain_module, "ModelType"):
-        pretrain(
-            pretrain_module.train_valid_test_datasets_provider,
-            pretrain_module.model_provider,
-            pretrain_module.ModelType.encoder_or_decoder,
-            pretrain_module.forward_step,
-            data_post_process=pretrain_module.data_post_process,
-            external_args=megatron_config,
-        )
-    elif hasattr(pretrain_module, "llama_argument_handler"):
-        pretrain(
-            pretrain_module.train_valid_test_datasets_provider,
-            pretrain_module.model_provider,
-            pretrain_module.forward_step,
-            pretrain_module.llama_argument_handler,
-            external_args=megatron_config,
-        )
+            
+    common.init(config)
+    megatron_config = config.get('megatron_config', {})
+    
+    if hasattr(pretrain_module, 'ModelType'):
+        pretrain(pretrain_module.train_valid_test_datasets_provider,
+                 pretrain_module.model_provider,
+                 pretrain_module.ModelType.encoder_or_decoder,
+                 pretrain_module.forward_step,
+                 args_defaults=megatron_config,
+                 data_post_process=pretrain_module.data_post_process)
+    elif hasattr(pretrain_module, 'llama_argument_handler'):
+        pretrain(pretrain_module.train_valid_test_datasets_provider,
+                 pretrain_module.model_provider,
+                 pretrain_module.forward_step,
+                 pretrain_module.llama_argument_handler,
+                 args_defaults=megatron_config)
     else:
-        pretrain(
-            pretrain_module.train_valid_test_datasets_provider,
-            pretrain_module.model_provider,
-            pretrain_module.forward_step,
-            external_args=megatron_config,
-        )
-
+        pretrain(pretrain_module.train_valid_test_datasets_provider,
+                 pretrain_module.model_provider,
+                 pretrain_module.forward_step,
+                 args_defaults=megatron_config)
 
 def main(external_config=None):
     config = common.Config()
