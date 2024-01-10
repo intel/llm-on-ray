b6b4c4e748bc83d9979c08f48725833f773dab62
Deegue <zyzzxycj@163.com>
2024-01-10 03:16:57 +0000

    remove iter

--- examples/inference/api_server_simple/query_single.py
+++ examples/inference/api_server_simple/query_single.py
@@ -25,7 +25,6 @@ parser.add_argument("--max_new_tokens", default=None, help="The maximum numbers 
 parser.add_argument("--temperature", default=None, help="The value used to modulate the next token probabilities.")
 parser.add_argument("--top_p", default=None, help="If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to`Top p` or higher are kept for generation.")
 parser.add_argument("--top_k", default=None, help="The number of highest probability vocabulary tokens to keep for top-k-filtering.")
-parser.add_argument("--num_iter", default=10, type=int, help="The number of inference iterations.")
 
 args = parser.parse_args()
 prompt = "Once upon a time,"
@@ -41,15 +40,12 @@ if args.top_k:
 
 sample_input = {"text": prompt, "config": config, "stream": args.streaming_response}
 
-num_iter = args.num_iter
-for i in range(num_iter):
-    print("iter: ", i)
-    proxies = { "http": None, "https": None}
-    outputs = requests.post(args.model_endpoint, proxies=proxies, json=sample_input, stream=args.streaming_response)
-    if args.streaming_response:
-        outputs.raise_for_status()
-        for output in outputs.iter_content(chunk_size=None, decode_unicode=True):
-            print(output, end='', flush=True)
-        print()
-    else:
-        print(outputs.text, flush=True)
+proxies = { "http": None, "https": None}
+outputs = requests.post(args.model_endpoint, proxies=proxies, json=sample_input, stream=args.streaming_response)
+if args.streaming_response:
+    outputs.raise_for_status()
+    for output in outputs.iter_content(chunk_size=None, decode_unicode=True):
+        print(output, end='', flush=True)
+    print()
+else:
+    print(outputs.text, flush=True)
