c6f0dc9e6e2caac8bfa15417f19dc12cf493fee4
Wu, Xiaochang <xiaochang.wu@intel.com>
2024-01-10 09:06:05 +0000

    nit
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

--- dev/scripts/install-vllm-cpu.sh
+++ dev/scripts/install-vllm-cpu.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+# Create conda env
+# conda create -n vllm-cpu python=3.10
+# conda activate vllm-cpu
+
+# Install g++ 12.3 for building
 conda install -y -c conda-forge gxx=12.3 gxx_linux-64=12.3
+
+# Install from source
+# TODO: need to verify if conda env needed to reactivate to setup g++ envs
 MAX_JOBS=8 pip install -v git+https://github.com/bigPYJ1151/vllm@PR_Branch
