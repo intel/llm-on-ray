7573a7b9dc611ccb3be678e7c6f121f0c1ad8b69
Wu, Xiaochang <xiaochang.wu@intel.com>
2024-01-10 08:03:27 +0000

    nit
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

--- dev/scripts/install-vllm-cpu.sh
+++ dev/scripts/install-vllm-cpu.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 conda install -y -c conda-forge gxx=12.3 gxx_linux-64=12.3
-pip install torch==2.1.0+cpu --index-url https://download.pytorch.org/whl/cpu
 MAX_JOBS=8 pip install -v git+https://github.com/bigPYJ1151/vllm@PR_Branch
