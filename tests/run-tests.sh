d4fc440f46418e1c80d9da9d1a4d52730e455805
yutianchen666 <tianchen.yu@intel.com>
2024-01-15 09:33:21 +0800

    fix review

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
 
-# This script runs pytest to execute all tests in the test_utils.py file.
-
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
 # Run pytest with the test file
 pytest -vs ./inference
 
