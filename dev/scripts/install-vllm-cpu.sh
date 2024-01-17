3f9ba5774e9ffae7586c7b57e2860b89500c9c27
Wu, Xiaochang <xiaochang.wu@intel.com>
2024-01-17 13:10:08 +0000

    fix

--- dev/scripts/install-vllm-cpu.sh
+++ dev/scripts/install-vllm-cpu.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# g++ version should be >=12.3
+g++ --version
+
 # Install from source
-MAX_JOBS=8 pip install -v git+https://github.com/bigPYJ1151/vllm@PR_Branch -f https://download.pytorch.org/whl/torch_stable.html
+MAX_JOBS=8 pip install -v git+https://github.com/bigPYJ1151/vllm@PR_Branch \
+    -f https://download.pytorch.org/whl/torch_stable.html
