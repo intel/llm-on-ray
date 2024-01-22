5a4a28ca9058fed7945b7515a7adc5d2359f6072
Wu, Xiaochang <xiaochang.wu@intel.com>
2024-01-22 11:49:27 +0000

    update

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -5,7 +5,7 @@ import asyncio
 import json
 import random
 import time
-from typing import AsyncGenerator, List, Tuple
+from typing import AsyncGenerator, Dict, List, Tuple, Union
 
 import aiohttp
 import numpy as np
@@ -46,8 +46,6 @@ def sample_requests(
         prompt_len = len(prompt_token_ids)
         if prompt_len < 4 or output_len < 4:
             # Prune too short sequences.
-            # This is because TGI causes errors when the input or output length
-            # is too short.
             continue
         if prompt_len > 1024 or prompt_len + output_len > 2048:
             # Prune too long sequences.
@@ -76,21 +74,16 @@ async def get_request(
 
 
 async def send_request(
-    api_url: str,
-    prompt: str,
-    prompt_len: int,
-    output_len: int,
-    use_beam_search: bool,
+    api_url: str, prompt: str, prompt_len: int, output_len: int, config: dict
 ) -> None:
     request_start_time = time.perf_counter()
 
     headers = {"User-Agent": "Benchmark Client"}
-    config = {
-        "max_new_tokens": output_len,
-        # "temperature": None,
-        # "top_k": None,
-        # "top_p": None
-    }
+
+    # Use sample output_len if max_new_tokens not specified
+    if config["max_new_tokens"] is None:
+        config["max_new_tokens"] = output_len
+
     pload = {
         "text": prompt,
         "config": config,
@@ -109,29 +102,18 @@ async def send_request(
             print(f"=== Response ===\n{output}\n================\n")
             break
 
-            # TODO: current output is pure text, better to use json instead
-            # output = json.loads(output)
-            # # Re-send the request if it failed.
-            # if "error" not in output:
-            #     break
-
     request_end_time = time.perf_counter()
     request_latency = request_end_time - request_start_time
     REQUEST_LATENCY.append((prompt_len, output_len, request_latency))
 
 
 async def benchmark(
-    api_url: str,
-    input_requests: List[Tuple[str, int, int]],
-    use_beam_search: bool,
-    request_rate: float,
+    api_url: str, input_requests: List[Tuple[str, int, int]], request_rate: float, config: dict
 ) -> None:
     tasks: List[asyncio.Task] = []
     async for request in get_request(input_requests, request_rate):
         prompt, prompt_len, output_len = request
-        task = asyncio.create_task(
-            send_request(api_url, prompt, prompt_len, output_len, use_beam_search)
-        )
+        task = asyncio.create_task(send_request(api_url, prompt, prompt_len, output_len, config))
         tasks.append(task)
     await asyncio.gather(*tasks)
 
@@ -152,30 +134,45 @@ def main(args: argparse.Namespace):
 
     input_requests = sample_requests(args.dataset, args.num_prompts, tokenizer)
 
+    config: Dict[str, Union[int, float]] = {}
+
+    if args.max_new_tokens:
+        config["max_new_tokens"] = int(args.max_new_tokens)
+    if args.temperature:
+        config["temperature"] = float(args.temperature)
+    if args.top_p:
+        config["top_p"] = float(args.top_p)
+    if args.top_k:
+        config["top_k"] = float(args.top_k)
+
     benchmark_start_time = time.perf_counter()
-    asyncio.run(benchmark(api_url, input_requests, args.use_beam_search, args.request_rate))
+    asyncio.run(benchmark(api_url, input_requests, args.request_rate, config))
     benchmark_end_time = time.perf_counter()
     benchmark_time = benchmark_end_time - benchmark_start_time
     print(f"Total time: {benchmark_time:.2f} s")
 
-    # Compute the throughput statistics.
+    # Compute prompts statistics
+    prompt_lens = [prompt_len for prompt_len, _, _ in REQUEST_LATENCY]
+    min_prompt_len = np.min(prompt_lens)
+    med_prompt_len = np.median(prompt_lens)
+    max_prompt_len = np.max(prompt_lens)
+    print(f"Prompt Length (Min/Med/Max): {min_prompt_len} / {med_prompt_len} / {max_prompt_len}")
+
+    # Compute the throughput statistics
     total_num_tokens = sum(prompt_len + output_len for prompt_len, output_len, _ in REQUEST_LATENCY)
     print(
         f"Throughput: {args.num_prompts / benchmark_time:.2f} requests/s, "
         f"{total_num_tokens / benchmark_time:.2f} tokens/s"
     )
 
-    # Compute the latency statistics.
+    # Compute the latency statistics
     avg_latency = np.mean([latency for _, _, latency in REQUEST_LATENCY])
     print(f"Average latency: {avg_latency:.2f} s")
+
     avg_per_token_latency = np.mean(
         [latency / (prompt_len + output_len) for prompt_len, output_len, latency in REQUEST_LATENCY]
     )
     print(f"Average latency per token: {avg_per_token_latency:.2f} s")
-    avg_per_output_token_latency = np.mean(
-        [latency / output_len for _, output_len, latency in REQUEST_LATENCY]
-    )
-    print("Average latency per output token: " f"{avg_per_output_token_latency:.2f} s")
 
 
 if __name__ == "__main__":
@@ -207,10 +204,33 @@ if __name__ == "__main__":
         "Otherwise, we use Poisson process to synthesize "
         "the request arrival times.",
     )
-    parser.add_argument("--use-beam-search", action="store_true")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument(
         "--trust-remote-code", action="store_true", help="trust remote code from huggingface"
     )
+
+    parser.add_argument(
+        "--max_new_tokens",
+        default=None,
+        help="The maximum numbers of tokens to generate. dataset sample output length is used when not specified.",
+    )
+    parser.add_argument(
+        "--temperature",
+        default=None,
+        help="The value used to modulate the next token probabilities.",
+    )
+    parser.add_argument(
+        "--top_p",
+        default=None,
+        help="If set to float < 1, only the smallest set of most probable tokens \
+            with probabilities that add up to `Top p` or higher are kept for generation.",
+    )
+    parser.add_argument(
+        "--top_k",
+        default=None,
+        help="The number of highest probability vocabulary tokens to keep \
+            for top-k-filtering.",
+    )
+
     args = parser.parse_args()
     main(args)
--- benchmarks/start-serving.sh
+++ benchmarks/start-serving.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-LLM_RAY_HOME=$(cd $(dirname $0)/.. && pwd -P)
-echo LLM_RAY_HOME is $LLM_RAY_HOME
+LLM_ON_RAY_HOME=$(cd $(dirname $0)/.. && pwd -P)
+echo LLM_ON_RAY_HOME is $LLM_ON_RAY_HOME
 
-cd $LLM_RAY_HOME
+cd $LLM_ON_RAY_HOME
 
-MODEL_ID=llama2_7b
-# MODEL_ID=gpt-j-6B
-
-export TRANSFORMERS_OFFLINE=1
-export HF_DATASETS_OFFLINE=1
+# export TRANSFORMERS_OFFLINE=1
+# export HF_DATASETS_OFFLINE=1
 
 export NUMEXPR_MAX_THREADS=$(nproc)
 
