972b488b6e7c5982e7fae80ad6ef5c9e038944ec
yutianchen666 <tianchen.yu@intel.com>
2024-02-01 13:59:56 +0000

    change to dare&docker

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,6 +2,6 @@
 cd $(dirname $0)
 
 # Run pytest with the test file
-pytest -vs --capture=tee-sys  ./inference
+pytest -vs --capture=tee-sys --show-capture=all ./inference
 
 echo "Pytest finished running tests."
