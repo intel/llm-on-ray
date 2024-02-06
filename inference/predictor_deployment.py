8baef62573e47c64661f0c6be988fbb06c746930
KepingYan <keping.yan@intel.com>
2024-02-06 17:27:40 +0800

    [UI] Add ui install script & fix ui (#101)
    * add ui install script & fix ui
    
    * fix stop_words
    
    * remove metrics related code
    
    * remove metrics related code

--- inference/predictor_deployment.py
+++ inference/predictor_deployment.py
@@ -28,7 +28,7 @@ from typing import Union, Dict, Any
 from starlette.responses import StreamingResponse, JSONResponse
 from fastapi import HTTPException
 from inference.api_openai_backend.openai_protocol import ModelResponse
-from utils import get_prompt_format, PromptFormat
+from inference.utils import get_prompt_format, PromptFormat
 
 
 @serve.deployment
