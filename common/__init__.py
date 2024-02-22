5f6dab093369b5ed98011225aa48d5978cf36b7e
Zhang Jian <jian2.zhang@intel.com>
2024-02-22 14:28:50 +0800

    add necessary module

--- common/__init__.py
+++ common/__init__.py
@@ -4,6 +4,8 @@ from . import agentenv
 from .torch_config import TorchConfig  # noqa: F401
 from typing import Dict, Any
 import sys
+from .config import Config
+from .init import init
 
 
 @load_check_decorator  # noqa: F405 # may be undefined, or defined from star imports
