a058bb416e4007f3a636484af5610dc413acb1d0
yutianchen666 <tianchen.yu@intel.com>
2024-02-23 16:12:14 +0000

    fix openai

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd $(dirname $0)
 
+export OPENAI_BASE_URL=http://localhost:8000/v
+export OPENAI_API_BASE=http://localhost:8000/v
+export OPENAI_API_KEY=not_a_real_key
+export no_proxy=localhost,127.0.0.1
+
 # Run pytest with the test file
 pytest -vs --capture=tee-sys --show-capture=all ./inference
 
