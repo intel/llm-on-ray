4136345d56a675ddc3600bb63b454b715c18fa50
yutianchen666 <tianchen.yu@intel.com>
2024-02-26 16:13:11 +0000

    codepath

--- .github/workflows/config/docker.sh
+++ .github/workflows/config/docker.sh
@@ -18,3 +18,39 @@ fuction build_and_prune() {
     yes | docker container prune && \
     yes | docker image prune -f
 }
+
+fuction run_docker() {
+    local TARGET="$1" 
+    local DF_SUFFIX="$2"  
+    local http_proxy="$3"
+    local https_proxy="$4"
+    local model_cache_path="$5"
+    local code_checkout_path="$6"
+
+    cid=$(docker ps -q --filter "name=${TARGET}${DF_SUFFIX}")
+    if [[ ! -z "$cid" ]]; then
+        docker stop "$cid" && docker rm "$cid"
+    fi
+
+    # check and remove exited container
+    cid=$(docker ps -a -q --filter "name=${TARGET}${DF_SUFFIX}")
+    if [[ ! -z "$cid" ]]; then
+        docker rm "$cid"
+    fi
+
+    docker run -tid \
+        -v "${{model_cache_path }}:/root/.cache/huggingface/hub" \
+        -v "${{code_checkout_path }}:/root/llm-on-ray" \
+        -e http_proxy="${{http_proxy }}" \
+        -e https_proxy="${{https_proxy }}" \
+        --name="${TARGET}${DF_SUFFIX}" \
+        --hostname="${TARGET}-container" \
+        "${TARGET}:latest"
+}
+
+fuction docker_bash(){
+    local TARGET="$1" 
+    local bash_command="$2"
+
+    docker exec "${TARGET}" bash -c "${bash_command}"
+}
