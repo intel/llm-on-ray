9dcfa19110a79990dd314851e4ecf29cec31b0a8
yutianchen666 <tianchen.yu@intel.com>
2024-02-26 14:17:09 +0000

    reduce req

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,11 +2,6 @@
 set -eo pipefail
 cd $(dirname $0)
 
-export OPENAI_BASE_URL=http://localhost:8000/v
-export OPENAI_API_BASE=http://localhost:8000/v
-export OPENAI_API_KEY=not_a_real_key
-export no_proxy=localhost,127.0.0.1
-
 # Run pytest with the test file
 pytest -vv --capture=tee-sys --show-capture=all ./inference
 
