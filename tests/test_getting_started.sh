36126da1e98c9a16483f152a47ebef4894470bac
Deegue <zyzzxycj@163.com>
2024-03-15 05:27:51 +0000

    fix

--- tests/test_getting_started.sh
+++ tests/test_getting_started.sh
@@ -20,17 +20,20 @@ pip install .[cpu] -f https://developer.intel.com/ipex-whl-stable-cpu -f https:/
 # Dynamic link oneCCL and Intel MPI libraries
 source $(python -c "import oneccl_bindings_for_pytorch as torch_ccl;print(torch_ccl.cwd)")/env/setvars.sh
 
-# Step 2: Serving
-# take gpt2 for example
-echo "Step 2: Serving"
-echo "Starting ray server for gpt2 with 3 cpu per worker"
-llm_on_ray-serve --config_file .github/workflows/config/gpt2-ci.yaml --simple --cpus_per_worker 3 
+# Step 2: Start ray cluster
+ray start --head
 
-# Step 3: access OpenAI API
+# Step 3: Serving
+# take gpt2 for example
+echo "Step 3: Serving"
+echo "Starting ray server for gpt2 with 3 cpu per worker"
+llm_on_ray-serve --config_file .github/workflows/config/gpt2-ci.yaml
+
+# Step 4: Access OpenAI API
 # 1.Using curl
-echo "Step 3: access OpenAI API"
+echo "Step 4: access OpenAI API"
 echo "Way 1: Using curl to access model"
-export ENDPOINT_URL=http://0.0.0.0:8000/v1
+export ENDPOINT_URL=http://localhost:8000/v1
 curl $ENDPOINT_URL/chat/completions \
     -H "Content-Type: application/json" \
     -d '{
@@ -41,16 +44,23 @@ curl $ENDPOINT_URL/chat/completions \
 
 # 2.Using requests library
 echo "Way 2: Using requests library to access model"
-python examples/inference/api_server_openai/query_http_requests.py
+python examples/inference/api_server_openai/query_http_requests.py --model_name gpt2
 
 # 3.Using OpenAI SDK
 echo "Way 3: Using OpenAI SDK to access model"
-export OPENAI_BASE_URL=http://localhost:8000/v1
-export OPENAI_API_KEY="not_a_real_key"
-python examples/inference/api_server_openai/query_openai_sdk.py --model_name gpt2 
+export no_proxy="localhost,127.0.0.1"
+export OPENAI_API_BASE="http://localhost:8000/v1"
+export OPENAI_BASE_URL="http://localhost:8000/v1"
+export OPENAI_API_KEY="YOUR_OPEN_AI_KEY"
+python examples/inference/api_server_openai/query_openai_sdk.py --model_name gpt2 --streaming_response --max_new_tokens 128 --temperature 0.8
 
-# 4.Using serve.py and query_single.py
-# Access models by query_single.py
-# TODO: some bugs in query_single.py, enable it after fixed by follow-up PRs.
-# echo "Way 4: Using query_single.py to access model"
-# python examples/inference/api_server_simple/query_single.py --model_endpoint http://0.0.0.0:8000/gpt2
+# Step 5: Restart ray cluster and Serving with simple
+echo "Step 5: Restart ray cluster and Serving with simple"
+ray stop -f
+sleep 1
+ray start --head
+llm_on_ray-serve --config_file .github/workflows/config/gpt2-ci.yaml --simple
+
+# 4.Using serve.py and access models by query_single.py
+echo "Way 4: Using query_single.py to access model"
+python examples/inference/api_server_simple/query_single.py --model_endpoint http://localhost:8000/gpt2
