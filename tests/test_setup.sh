2aa7b2659b156067cb1814010b62b63c2960c62d
Deegue <zyzzxycj@163.com>
2024-03-15 05:55:08 +0000

    fix setup

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-# Usage: ./test_setup [CPU, GPU, Gaudi] [Deepspeed]
+# Usage: ./test_setup [CPU, GPU, Gaudi] [True for Deepspeed and false for disable]
+
+if [ "$#" != 2 ]; then
+    echo "Error, there should be 2 arguments! See Usage: ./test_setup [CPU, GPU, Gaudi] [True for Deepspeed and false for disable]"
+    exit 1
+fi
 
 # Step 1: Clone and create conda environment
 git clone https://github.com/intel/llm-on-ray.git
 cd llm-on-ray
-conda create -n llm-on-ray python=3.9
-conda activate llm-on-ray
 
 # Step 2: Check CPU, GPU or Gaudi and install dependencies
 hardware=0
@@ -30,23 +33,21 @@ case $(echo $1 | tr 'a-z' 'A-Z') in
 esac
 
 # Check if it neesd deepspeed
-if [ $(echo $2 | tr 'A-Z' 'a-z') == "deepspeed" ]
+if [ $(echo $2 | tr 'A-Z' 'a-z') == "true" ]
 then
     deepspeed=true
     source $(python -c "import oneccl_bindings_for_pytorch as torch_ccl;print(torch_ccl.cwd)")/env/setvars.sh
 fi
 
 # If Gaudi, build docker
-if [ "$hardware" = 3 ]
-then
+if [ "$hardware" == 3 ]; then
     export dockerfile=inference/habana/Dockerfile
+    export no_proxy="localhost,127.0.0.1"
     docker build \
-    -f ${dockerfile} ../../ \
-    -t llm-ray-habana:latest \
-    --network=host \
-    --build-arg http_proxy=${http_proxy} \
-    --build-arg https_proxy=${https_proxy} \
-    --build-arg no_proxy=${no_proxy} \
+        -f ${dockerfile} ../../ \
+        -t llm-ray-habana:latest \
+        --network=host \
+        --build-arg no_proxy=${no_proxy}
 fi
 
 
