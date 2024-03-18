d78952be95600ef4da8bdc59e637ad66fb2f8343
Deegue <zyzzxycj@163.com>
2024-03-18 01:49:37 +0000

    nit

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -36,7 +36,7 @@ esac
 if [ $(echo $2 | tr 'A-Z' 'a-z') == "true" ]
 then
     deepspeed=true
-    source $(python -c "import oneccl_bindings_for_pytorch as torch_ccl;print(torch_ccl.cwd)")/env/setvars.sh
+    source $(python -c "import oneccl_bindings_for_pytorch as torch_ccl; print(torch_ccl.cwd)")/env/setvars.sh
 fi
 
 # If Gaudi, build docker
