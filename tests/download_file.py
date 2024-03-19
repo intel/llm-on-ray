b9fffe039ba2007e5d8e7caa82ec60894d1ee8e4
yutianchen666 <tianchen.yu@intel.com>
2024-03-19 10:51:16 +0000

    delete redundant

--- tests/download_file.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="meta-llama/Llama-2-7b-chat-hf",
-    local_dir="./hg_cache/meta-llama/Llama-2-7b-chat-hf",
-    local_dir_use_symlinks=False,
-    token="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",
-)
-# snapshot_download(repo_id="meta-llama/Llama-2-7b-chat-hf")
