5337f66104c5c366d1c8a4219deb9085f0b2cd50
yutianchen666 <tianchen.yu@intel.com>
2024-03-22 10:41:01 +0000

    fix muti serve

--- tests/inference/test_example_query_http_requests.py
+++ tests/inference/test_example_query_http_requests.py
@@ -3,7 +3,8 @@ import pytest
 import os
 
 
-def script_with_args(model_name, streaming_response, max_new_tokens, temperature, top_p):
+def start_serve(model_name):
+    print("start_serve")
     current_path = os.path.dirname(os.path.abspath(__file__))
 
     config_path = os.path.join(
@@ -17,17 +18,18 @@ def script_with_args(model_name, streaming_response, max_new_tokens, temperature
     result_serve = subprocess.run(cmd_serve, capture_output=True, text=True)
 
     # Ensure there are no errors in the serve script execution
-    try:
-        assert result_serve.returncode == 0, print(
-            "\n" + "Serve error stderr message: " + "\n", result_serve.stderr
-        )
-    except AssertionError:
-        print("\n" + "Serve error stdout message: " + "\n", result_serve.stdout)
+    assert result_serve.returncode == 0, print(
+        "\n" + "Serve error stderr message: " + "\n", result_serve.stderr
+    )
 
     # Print the output of subprocess.run for checking if output is expected
     print("\n" + "Serve message: " + "\n", result_serve.stdout)
 
     assert "Error" not in result_serve.stderr
+
+
+def script_with_args(model_name, streaming_response, max_new_tokens, temperature, top_p):
+    current_path = os.path.dirname(os.path.abspath(__file__))
 
     example_http_path = os.path.join(
         current_path, "../../examples/inference/api_server_openai/query_http_requests.py"
@@ -55,10 +57,11 @@ def script_with_args(model_name, streaming_response, max_new_tokens, temperature
     result_http = subprocess.run(cmd_http, capture_output=True, text=True)
 
     # Ensure there are no errors in the http query script execution
-    try:
-        assert result_http.returncode == 0, print(result_serve.stderr)
-    except AssertionError:
-        print(result_http.stdout)
+    assert result_http.returncode == 0, print(
+        "\n" + "Http error stderr message: " + "\n", result_http.stderr
+    )
+
+    print("\n" + "Http output message: " + "\n", result_http.stdout)
 
     # Print the output of subprocess.run for checking if output is expected
     print("\n" + "Model in Http output message: " + "\n", result_http.stdout)
@@ -68,6 +71,9 @@ def script_with_args(model_name, streaming_response, max_new_tokens, temperature
     assert isinstance(result_http.stdout, str)
 
     assert len(result_http.stdout) > 0
+
+
+executed_models = {}
 
 
 @pytest.mark.parametrize(
@@ -82,4 +88,11 @@ def script_with_args(model_name, streaming_response, max_new_tokens, temperature
     ],
 )
 def test_script(model_name, streaming_response, max_new_tokens, temperature, top_p):
+    global executed_models
+
+    # Check if this modelname has already executed start_serve
+    if model_name not in executed_models:
+        start_serve(model_name)
+        # Mark this modelname has already executed start_serve
+        executed_models[model_name] = True
     script_with_args(model_name, streaming_response, max_new_tokens, temperature, top_p)
--- tests/inference/test_example_query_openai_sdk.py
+++ tests/inference/test_example_query_openai_sdk.py
@@ -8,7 +8,7 @@ os.environ["OPENAI_API_KEY"] = "YOUR_OPEN_AI_KEY"
 os.environ["OPENAI_BASE_URL"] = "http://localhost:8000/v1"
 
 
-def script_with_args(api_base, model_name, streaming_response, max_new_tokens, temperature, top_p):
+def start_serve(api_base, model_name):
     # Other OpenAI SDK tests
     if api_base != "http://localhost:8000/v1":
         os.environ["OPENAI_API_BASE"] = api_base
@@ -27,17 +27,18 @@ def script_with_args(api_base, model_name, streaming_response, max_new_tokens, t
     result_serve = subprocess.run(cmd_serve, capture_output=True, text=True)
 
     # Ensure there are no errors in the serve script execution
-    try:
-        assert result_serve.returncode == 0, print(
-            "\n" + "Serve error stderr message: " + "\n", result_serve.stderr
-        )
-    except AssertionError:
-        print("\n" + "Serve error stdout message: " + "\n", result_serve.stdout)
+    assert result_serve.returncode == 0, print(
+        "\n" + "Serve error stderr message: " + "\n", result_serve.stderr
+    )
 
     # Print the output of subprocess.run for checking if output is expected
     print("\n" + "Serve message: " + "\n", result_serve.stdout)
 
     assert "Error" not in result_serve.stderr
+
+
+def script_with_args(api_base, model_name, streaming_response, max_new_tokens, temperature, top_p):
+    current_path = os.path.dirname(os.path.abspath(__file__))
 
     example_openai_path = os.path.join(
         current_path, "../../examples/inference/api_server_openai/query_openai_sdk.py"
@@ -65,10 +66,7 @@ def script_with_args(api_base, model_name, streaming_response, max_new_tokens, t
     result_openai = subprocess.run(cmd_openai, capture_output=True, text=True)
 
     # Ensure there are no errors in the OpenAI API query script execution
-    try:
-        assert result_openai.returncode == 0, print(result_serve.stderr)
-    except AssertionError:
-        print(result_openai.stdout)
+    assert result_openai.returncode == 0, print(result_openai.stderr)
 
     # Print the output of subprocess.run for checking if output is expected
     print("\n" + "Model in Openai output message: " + "\n", result_openai.stdout)
@@ -78,6 +76,9 @@ def script_with_args(api_base, model_name, streaming_response, max_new_tokens, t
     assert isinstance(result_openai.stdout, str)
 
     assert len(result_openai.stdout) > 0
+
+
+executed_models = {}
 
 
 # Parametrize the test function with different combinations of parameters
@@ -94,4 +95,11 @@ def script_with_args(api_base, model_name, streaming_response, max_new_tokens, t
     ],
 )
 def test_script(api_base, model_name, streaming_response, max_new_tokens, temperature, top_p):
+    global executed_models
+
+    # Check if this modelname has already executed start_serve
+    if model_name not in executed_models:
+        start_serve(model_name)
+        # Mark this modelname has already executed start_serve
+        executed_models[model_name] = True
     script_with_args(api_base, model_name, streaming_response, max_new_tokens, temperature, top_p)
