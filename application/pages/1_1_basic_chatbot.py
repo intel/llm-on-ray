45420151f214fec484c385e7875d9ff65cba6fd0
Deegue <zyzzxycj@163.com>
2024-04-16 07:29:39 +0000

    fix lint

--- application/pages/1_1_basic_chatbot.py
+++ application/pages/1_1_basic_chatbot.py
@@ -22,29 +22,35 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 
 st.set_page_config(page_title="Chatbot", page_icon="💬")
-st.header('Basic Chatbot')
-st.write('Allows users to interact with the LLM')
+st.header("Basic Chatbot")
+st.write("Allows users to interact with the LLM")
+
 
 class Basic:
-
     def __init__(self):
         self.openai_model = "mistral-7b-instruct-v0.2"
-        self.history_messages = utils.enable_chat_history('basic_chat')
-    
+        self.history_messages = utils.enable_chat_history("basic_chat")
+
     def setup_chain(self):
-        llm = ChatOpenAI(openai_api_base = "http://localhost:8000/v1", model_name=self.openai_model, openai_api_key="not_needed", streaming=True, max_tokens=512)
+        llm = ChatOpenAI(
+            openai_api_base="http://localhost:8000/v1",
+            model_name=self.openai_model,
+            openai_api_key="not_needed",
+            streaming=True,
+            max_tokens=512,
+        )
         chain = ConversationChain(llm=llm, verbose=True)
         return chain
-    
+
     def main(self):
         chain = self.setup_chain()
-        for message in self.history_messages: # Display the prior chat messages
+        for message in self.history_messages:  # Display the prior chat messages
             with st.chat_message(message["role"]):
                 st.write(message["content"])
 
         if user_query := st.chat_input(placeholder="Ask me anything!"):
             self.history_messages.append({"role": "user", "content": user_query})
-            with st.chat_message('user'):
+            with st.chat_message("user"):
                 st.write(user_query)
 
             with st.chat_message("assistant"):
@@ -52,6 +58,7 @@ class Basic:
                     st_cb = StreamHandler(st.empty())
                     response = chain.run(user_query, callbacks=[st_cb])
                     self.history_messages.append({"role": "assistant", "content": response})
+
 
 if __name__ == "__main__":
     obj = Basic()
