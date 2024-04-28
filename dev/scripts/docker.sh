a4ba674d7d501218f350288d998f12ab557de479
yutianchen666 <tianchen.yu@intel.com>
2024-04-28 15:24:46 +0000

    test install error

--- dev/scripts/docker.sh
+++ dev/scripts/docker.sh
@@ -123,10 +123,56 @@ DF_SUFFIX_MAPPER=(
 
 declare -A TARGET_MAPPER
 TARGET_MAPPER=(
-    ["mpt-7b-ipex-llm"]="${target}_ipex-llm"
-    ["llama-2-7b-chat-hf-vllm"]="${target}_vllm"
-    ["default"]="${target}"
+    ["mpt-7b-ipex-llm"]="_ipex-llm"
+    ["llama-2-7b-chat-hf-vllm"]="_vllm"
+    ["default"]=""
 )
 
-model="mpt-7b-ipex-llm"
-target=${TARGET_MAPPER[$model]}
+declare -A INFERENCE_MAPPER
+INFERENCE_MAPPER=(
+    ["mpt-7b-ipex-llm"]="llm_on_ray-serve --config_file llm_on_ray/inference/models/ipex-llm/mpt-7b-ipex-llm.yaml --simple"
+    ["llama-2-7b-chat-hf-vllm"]="llm_on_ray-serve --config_file .github/workflows/config/llama-2-7b-chat-hf-vllm-fp32.yaml --simple"
+    ["default"]="llm_on_ray-serve --simple --models ${model}"
+)
+
+inference_test(){
+local TARGET=$1
+local model=$2
+
+docker exec "${TARGET}" bash -c "huggingface-cli login --token ${{env.HF_ACCESS_TOKEN}}"
+docker exec "${TARGET}" bash -c "${INFERENCE_MAPPER["${model}"]}"
+
+echo Non-streaming query:
+docker exec "${TARGET}" bash -c "python examples/inference/api_server_simple/query_single.py --model_endpoint http://127.0.0.1:8000/${model}"
+echo Streaming query:
+docker exec "${TARGET}" bash -c "python examples/inference/api_server_simple/query_single.py --model_endpoint http://127.0.0.1:8000/${model} --streaming_response"
+}
+
+inference_test_deltatuner(){
+    local TARGET=$1
+    local dtuner_model=$2
+    local model=$3
+    if [ -n "$dtuner_model" ];then
+        docker exec "${TARGET}" bash -c "llm_on_ray-serve --config_file .github/workflows/config/mpt_deltatuner.yaml --simple"
+        docker exec "${TARGET}" bash -c "python examples/inference/api_server_simple/query_single.py --model_endpoint http://127.0.0.1:8000/${model }"
+        docker exec "${TARGET}" bash -c "python examples/inference/api_server_simple/query_single.py --model_endpoint http://127.0.0.1:8000/${model } --streaming_response"
+    fi
+}
+
+
+inference_test_deepspeed(){
+    local TARGET=$1
+    local model=$2
+    if [[ ${model} =~ ^(gemma-2b|gpt2|falcon-7b|starcoder|mpt-7b.*)$ ]]; then
+        echo ${model} is not supported!
+    elif [[ ! ${model} == "llama-2-7b-chat-hf-vllm" ]]; then
+        docker exec "${TARGET}" bash -c "python .github/workflows/config/update_inference_config.py --config_file llm_on_ray/inference/models/\"${model}\".yaml --output_file \"${model}\".yaml.deepspeed --deepspeed"
+        docker exec "${TARGET}" bash -c "llm_on_ray-serve --config_file \"${model}\".yaml.deepspeed --simple"
+        docker exec "${TARGET}" bash -c "python examples/inference/api_server_simple/query_single.py --model_endpoint http://127.0.0.1:8000/${model}"
+        docker exec "${TARGET}" bash -c "python examples/inference/api_server_simple/query_single.py --model_endpoint http://127.0.0.1:8000/${model} --streaming_response"
+    fi
+}
+
+
+# model="mpt-7b-ipex-llm"
+# target=${TARGET_MAPPER[$model]}
