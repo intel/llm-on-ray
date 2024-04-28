f9c39450d7078ccb4ec24fc2871f29bd095da2ad
Wu, Xiaochang <xiaochang.wu@intel.com>
2024-04-28 02:13:52 +0000

    nit
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

--- llm_on_ray/inference/vllm_predictor.py
+++ llm_on_ray/inference/vllm_predictor.py
@@ -39,8 +39,6 @@ class VllmPredictor(Predictor):
         # The default value is 40MB.
         os.environ["VLLM_CPU_KVCACHE_SPACE"] = str(self.VLLM_CPU_KVCACHE_SPACE_DEFAULT)
 
-        # Remove device=infer_conf.device here as CPU device type dispatch is not aligned in vLLM now
-        # Will add back when this is fixed
         args = AsyncEngineArgs(
             model=model_desc.model_id_or_path,
             trust_remote_code=model_config.trust_remote_code,
