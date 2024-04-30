4a7ee831b147c7c036823b8cb7ef4368c6b61d2a
yutianchen666 <tianchen.yu@intel.com>
2024-04-30 15:38:23 +0000

    fix review

--- dev/scripts/docker.sh
+++ dev/scripts/docker.sh
@@ -6,12 +6,12 @@ HTTPS_PROXY='http://10.24.221.149:911'
 MODEL_CACHE_PATH_LOACL='/root/.cache/huggingface/hub'
 CODE_CHECKOUT_PATH_LOCAL='/root/llm-on-ray'
 
-build_and_prune_test() {
+build_and_prune() {
     # Set TARGET and DF-SUFFIX using the passed in parameters
     local TARGET=$1
     local DF_SUFFIX=$2
-    local PYTHON_V=$3  ## same name
-    local USE_PROXY=$4
+    local USE_PROXY=$3
+    local PYTHON_V=$4  
 
     docker_args=()
     docker_args+=("--build-arg=CACHEBUST=1")
@@ -32,27 +32,27 @@ build_and_prune_test() {
     docker image prune -f
  
 }
-build_and_prune() {
-    # Set TARGET and DF-SUFFIX using the passed in parameters
-    local TARGET=$1
-    local DF_SUFFIX=$2
-    local USE_PROXY=$3
+# build_and_prune() {
+#     # Set TARGET and DF-SUFFIX using the passed in parameters
+#     local TARGET=$1
+#     local DF_SUFFIX=$2
+#     local USE_PROXY=$3
 
-    docker_args=()
-    docker_args+=("--build-arg=CACHEBUST=1")
+#     docker_args=()
+#     docker_args+=("--build-arg=CACHEBUST=1")
 
-    if [ "$USE_PROXY" == "1" ]; then
-        docker_args+=("--build-arg=http_proxy=${HTTP_PROXY}")
-        docker_args+=("--build-arg=https_proxy=${HTTPS_PROXY}")
-    fi
+#     if [ "$USE_PROXY" == "1" ]; then
+#         docker_args+=("--build-arg=http_proxy=${HTTP_PROXY}")
+#         docker_args+=("--build-arg=https_proxy=${HTTPS_PROXY}")
+#     fi
     
-    echo "docker build ./ ${docker_args[@]} -f dev/docker/Dockerfile${DF_SUFFIX} -t ${TARGET}:latest && yes | docker container prune && yes | docker image prune -f"
+#     echo "docker build ./ ${docker_args[@]} -f dev/docker/Dockerfile${DF_SUFFIX} -t ${TARGET}:latest && yes | docker container prune && yes | docker image prune -f"
 
-    # Build Docker image and perform cleaning operation
-    docker build ./ "${docker_args[@]}" -f dev/docker/Dockerfile${DF_SUFFIX} -t ${TARGET}:latest && yes | docker container prune && yes 
-    docker image prune -f
+#     # Build Docker image and perform cleaning operation
+#     docker build ./ "${docker_args[@]}" -f dev/docker/Dockerfile${DF_SUFFIX} -t ${TARGET}:latest && yes | docker container prune && yes 
+#     docker image prune -f
  
-}
+# }
 
 start_docker() {
     local TARGET=$1
@@ -87,7 +87,11 @@ start_docker() {
 
     echo "docker run -tid  "${docker_args[@]}" "${TARGET}:latest""
     docker run -tid  "${docker_args[@]}" "${TARGET}:latest"
-    docker exec "${TARGET}" bash -c "huggingface-cli login --token ${HF_TOKEN}"
+    if [ -z "$HF_TOKEN" ]; then 
+        echo "no hf token" 
+    else 
+        docker exec "${TARGET}" bash -c "huggingface-cli login --token ${HF_TOKEN}"
+    fi
 }
 
 install_dependencies(){
