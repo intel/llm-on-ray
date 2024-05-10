dce05fb57a6ef0dd0a0cb458f63a3b3389822145
yutianchen <tianchen.yu@intel.com>
2024-05-10 15:06:16 +0800

    [Workflow]  Unify Docker operations into bash (#123)
    * docker2sh test
    
    * codepath
    
    * codepath
    
    * codepath
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * df
    
    * docker.sh
    
    * docker bash
    
    * docker bash
    
    * docker bash
    
    * docker bash
    
    * inference docker bash
    
    * merge main0312
    
    * merge main0312
    
    * merge main0312
    
    * test set-e
    
    * fix test
    
    * fix
    
    * fix
    
    * fix
    
    * test error
    
    * test error
    
    * add map
    
    * test install error
    
    * test install error
    
    * test install error
    
    * test install error
    
    * test
    
    * test
    
    * fix
    
    * fix
    
    * fix
    
    * only inference
    
    * fux
    
    * fux
    
    * fux
    
    * target
    
    * target
    
    * target
    
    * fix proxy
    
    * fix proxy
    
    * fix proxy
    
    * fix proxy
    
    * fix proxy
    
    * fix proxy
    
    * fix proxy
    
    * fix fuc
    
    * fix fuc
    
    * fix fuc
    
    * all inference
    
    * add finetune
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix finetune
    
    * fix finetune
    
    * fix review
    
    * fix review
    
    * fix review
    
    * add info output
    
    * Update proxy settings and Docker configurations
    
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>
    
    * fix vllm pr212
    
    * fix
    
    * fix
    
    * change name
    
    ---------
    
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>
    Co-authored-by: Wu, Xiaochang <xiaochang.wu@intel.com>

---
+++ dev/scripts/patch_yaml_config.py
@@ -0,0 +1,89 @@
+#
+# Copyright 2023 The LLM-on-Ray Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import yaml
+
+
+def patch_yaml_config():
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--conf_path", type=str)
+    parser.add_argument("--models", type=str)
+    parser.add_argument("--peft_lora", action="store_true", default=False)
+    parser.add_argument("--denas_lora", action="store_true", default=False)
+    args = parser.parse_args()
+
+    conf_path = args.conf_path
+    with open(conf_path, encoding="utf-8") as reader:
+        result = yaml.load(reader, Loader=yaml.FullLoader)
+        result["General"]["base_model"] = args.models
+        if args.models == "mosaicml/mpt-7b":
+            result["General"]["config"]["trust_remote_code"] = True
+        else:
+            result["General"]["config"]["trust_remote_code"] = False
+        if args.models == "EleutherAI/gpt-j-6b" or args.models == "gpt2":
+            result["General"]["gpt_base_model"] = True
+        else:
+            result["General"]["gpt_base_model"] = False
+        result["Training"]["epochs"] = 1
+        if args.models == "gpt2":
+            # to verify oneccl
+            result["Training"]["num_training_workers"] = 2
+        else:
+            result["Training"]["num_training_workers"] = 1
+
+        if args.peft_lora:
+            result["General"]["lora_config"] = {
+                "task_type": "CAUSAL_LM",
+                "r": 8,
+                "lora_alpha": 32,
+                "lora_dropout": 0.1,
+            }
+            if args.models == "mistralai/Mistral-7B-v0.1":
+                result["General"]["lora_config"]["target_modules"] = [
+                    "q_proj",
+                    "k_proj",
+                    "v_proj",
+                    "o_proj",
+                    "gate_proj",
+                    "up_proj",
+                    "down_proj",
+                    "lm_head",
+                ]
+            elif args.models == "google/gemma-2b":
+                result["General"]["lora_config"]["target_modules"] = ["k_proj", "v_proj"]
+            else:
+                result["General"]["lora_config"]["target_modules"] = None
+        else:
+            result["General"]["lora_config"] = None
+
+        if args.denas_lora:
+            os.system(
+                "cp -r $(python -m pip show deltatuner | grep Location | cut -d: -f2)/deltatuner/conf/best_structure examples/"
+            )
+            result["General"]["deltatuner_config"] = {
+                "algo": "lora",
+                "denas": True,
+                "best_model_structure": f"examples/best_structure/${args.models}-best_structure.jsonl",
+            }
+
+    with open(conf_path, "w") as output:
+        yaml.dump(result, output, sort_keys=False)
+
+
+if __name__ == "__main__":
+    patch_yaml_config()
