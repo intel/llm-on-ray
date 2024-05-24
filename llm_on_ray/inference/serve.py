396b46ebc6f6702e8d610a69886a013627e641e0
yutianchen666 <tianchen.yu@intel.com>
2024-05-24 16:49:35 +0000

    fix

--- llm_on_ray/inference/serve.py
+++ llm_on_ray/inference/serve.py
@@ -24,7 +24,6 @@ from llm_on_ray.inference.inference_config import (
     ModelDescription,
     InferenceConfig,
     all_models,
-    model_ids,
 )
 
 
@@ -129,10 +128,11 @@ def main(argv=None):
 
     args = parser.parse_args(argv)
 
+    all_models_name = list(all_models.keys())
     if args.list_model_ids:
-        for model in model_ids:
+        for model in all_models_name:
             print(model)
-            print(model_ids[model])
+            print("config_file_path:" + "llm_on_ray/inference/models/" + model + ".yaml")
         sys.exit(0)
 
     ray.init(address="auto")
