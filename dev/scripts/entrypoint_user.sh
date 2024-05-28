c804587c07a525abcf3c59c961f6133791968b82
yutianchen666 <tianchen.yu@intel.com>
2024-05-28 11:04:18 +0000

    add git

--- dev/scripts/entrypoint_user.sh
+++ dev/scripts/entrypoint_user.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
+set -e
+#!/bin/bash
 
-su 
-# Default model path
-DEFAULT_MODEL_PATH="/app/models/default_model"
-
-# Set proxies if needed
-
-export http_proxy=${http_proxy:-http://10.24.221.169:911}
-export https_proxy=${https_proxy:-http://10.24.221.169:911}
-
-# # Default volume mounts if needed
-# VOLUME_SOURCE1=${VOLUME_SOURCE1:-/home/yutianchen/Project/pr_lib/llm-on-ray}
-# VOLUME_TARGET1=${VOLUME_TARGET1:-/root/llm-on-ray}
-
-VOLUME_SOURCE2=${VOLUME_SOURCE2:-/home/yutianchen/.cache/huggingface/hub}
-VOLUME_TARGET2=${VOLUME_TARGET2:-/root/.cache/huggingface/hub}
-
-# Mount volumes
-mkdir -p /root/llm-on-ray
-mkdir -p /root/.cache/huggingface/hub
-mount --bind "$VOLUME_SOURCE1" "$VOLUME_TARGET1"
-mount --bind "$VOLUME_SOURCE2" "$VOLUME_TARGET2"
-
-# Check if any arguments are provided
-if [ "$#" -gt 0 ]; then
-    # If arguments are provided, use them
-    exec "$@"
+# Check if an environment variable exists and print its value
+if [ -n "$hf_token" ]; then
+  echo "The hf_token environment variable is: $hf_token"
+  # Execute Hugging Face CLI login command
+  huggingface-cli login --token "${hf_token}"
 else
-    # If no arguments are provided, start the default model service
-    echo "No arguments provided, starting default model service..."
-    # exec python serve_model.py --model-path $DEFAULT_MODEL_PATH
+  echo "Environment variable 'hf_token' is not set."
 fi
+
+# Default serve cmd
+ray start --head
+
+if [ -n "$MODEL_NAME"]; then
+    echo "Using User Model: $MODEL_NAME"
+    llm_on_ray-serve --models $MODEL_NAME
+else
+    echo "Using Default Model: gpt2"
+    llm_on_ray-serve --config_file llm_on_ray/inference/models/gpt2.yaml 
+fi
+
