3ce7d76fd7b0c63664fdef14aeb07ba33daf1d4c
minmingzhu <minming.zhu@intel.com>
2024-06-17 16:20:17 +0800

    update CI
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- llm_on_ray/finetune/finetune.py
+++ llm_on_ray/finetune/finetune.py
@@ -300,7 +300,7 @@ def get_trainer(config: Dict, model, tokenizer, tokenized_datasets, data_collato
     device = config["Training"]["device"]
     use_dpo = False
     if config["Training"]["finetuning_model"] is not None:
-        use_dpo = config["Training"]["FinetuningModel"].get("dpo", False)
+        use_dpo = config["Training"]["finetuning_model"].get("dpo", False)
     if device in ["cpu", "gpu"]:
         from transformers import Trainer, TrainingArguments
 
