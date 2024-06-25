e65a511e7359a35b769a21ef052673aca3869f19
minmingzhu <minming.zhu@intel.com>
2024-06-25 16:50:22 +0800

    using intel-oneapi-ccl-devel-2021.12 intel-oneapi-compiler-dpcpp-cpp-runtime-2024.1
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/scripts/install-oneapi.sh
+++ dev/scripts/install-oneapi.sh
@@ -9,6 +9,6 @@ wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCT
   echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
 
 apt update
-apt-get install -y intel-oneapi-ccl-devel intel-oneapi-compiler-dpcpp-cpp-runtime
+apt-get install -y intel-oneapi-ccl-devel-2021.12 intel-oneapi-compiler-dpcpp-cpp-runtime-2024.1
 
 echo "oneAPI components installed!"
