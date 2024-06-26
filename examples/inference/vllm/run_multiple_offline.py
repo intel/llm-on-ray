423fc107fad868d0b1ede75730e7126acd0982c0
Jiafu Zhang <jiafu.zhang@intel.com>
2024-06-26 09:32:25 +0000

    modified some examples
    Signed-off-by: Jiafu Zhang <jiafu.zhang@intel.com>

--- examples/inference/vllm/run_multiple_offline.py
+++ examples/inference/vllm/run_multiple_offline.py
@@ -1,2 +1,7 @@
-NS_NUM_THREADS=25 OMP_NUM_THREADS=1 OMP_PROC_BIND=true NS_MODEL_PERF_STEPS=50 nohup numactl -N 1 -m 1 -C 56-83   python vllm_offline_inference.py > 1.log 2>&1 &
-NS_NUM_THREADS=25 OMP_NUM_THREADS=1 OMP_PROC_BIND=true NS_MODEL_PERF_STEPS=50 nohup numactl -N 0 -m 0 -C 0-45   python vllm_offline_inference.py > 2.log 2>&1 &
+# two sockets
+#NS_NUM_THREADS=25 OMP_NUM_THREADS=1 OMP_PROC_BIND=true NS_MODEL_PERF_STEPS=50 nohup numactl -N 0 -m 0 -C 0-27   python vllm_offline_inference.py > 2.log 2>&1 &
+#NS_NUM_THREADS=25 OMP_NUM_THREADS=1 OMP_PROC_BIND=true NS_MODEL_PERF_STEPS=50 nohup numactl -N 1 -m 1 -C 56-83   python vllm_offline_inference.py > 1.log 2>&1 &
+
+TOKENIZERS_PARALLELISM=false NS_NUM_THREADS=25 OMP_NUM_THREADS=1 OMP_PROC_BIND=true NS_MODEL_PERF_STEPS=50 nohup numactl -N 0 -m 0 -C 0-26   python vllm_offline_inference.py > 2.log 2>&1 &
+TOKENIZERS_PARALLELISM=false NS_NUM_THREADS=25 OMP_NUM_THREADS=1 OMP_PROC_BIND=true NS_MODEL_PERF_STEPS=50 nohup numactl -N 0 -m 0 -C 27-53   python vllm_offline_inference.py > 1.log 2>&1 &
+
